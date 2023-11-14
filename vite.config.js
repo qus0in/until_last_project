3e289e5019a211f05a2c141f24b7ffdea953de9c
qus0in <qus0in@gmail.com>
2023-11-14 15:21:28 +0900

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(({ command }) => {
+  if (command == "build") {
+    return {
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+      },
+      base: "/until_last_project/",
+    };
+  } else {
+    return {
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+      },
+    };
   }
-})
+});
